23c40cfd933f0676525858f1175f54f809ba0b05
Emmanuel <eeolannye@gmail.com>
2025-11-20 13:22:59 -0600

    Made a constructor JavaScript file. Added it to the script folder

---
+++ assets/scripts/constructor.js
@@ -0,0 +1,19 @@
+'use strict';
+// Create a class named Score with 3 private properties (date, hits, percentage), 3 getters and a constructor). Score objects are created when the game ends.
+
+class Score {
+    #date;
+    #hits = 0;
+    #percentage;
+
+    constructor(date, hits, percentage) {
+        this.#date = date;
+        this.#hits = hits;
+        this.#percentage = percentage;
+    }
+
+    get date() { return this.#date; }
+    get hits() { return this.#hits; }
+    get percentage() { return this.#percentage; }
+    
+}
