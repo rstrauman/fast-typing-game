fee820e18cf0224e44404cf0c6c1c0e446884469
Riley <riley.s2003@hotmail.com>
2025-11-21 12:58:34 -0600

    added function to shuffle words array

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'];
+
+function sortWords(shuffleWords) {
+    let currentIndex = shuffleWords.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = shuffleWords[currentIndex];
+    shuffleWords[currentIndex] = shuffleWords[randomIndex];
+    shuffleWords[randomIndex] = temporaryValue;
+  }
+
+  return shuffleWords;
+}
+
+console.log(sortWords(words));
