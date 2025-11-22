51c0ee2548812736be11f9902d8ae595c65cbbf2
Riley <riley.s2003@hotmail.com>
2025-11-22 13:04:10 -0600

    finished early game start/restart logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,13 +2,16 @@
 
 let words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'];
 let randomWordArray = [];
+let nextWordCount = "";
 
-let userInput; // eventually will 
+// let userInput; // eventually will 
 let nextWord;
+let wordInputSpace;
 
 let gameContainer = document.querySelector('.game-container');
 let wordContainer = document.querySelector('.current-word-container');
 let startBtn = document.querySelector('.start-btn');
+let userInput = document.querySelector('.user-input');
 
 
 function sortWords(shuffleWords) {
@@ -32,14 +35,26 @@ function gameStart(){
     startBtn.remove();
 
     nextWord = document.createElement('div');
-    nextWord.innerHTML = randomWordArray[0];
+    nextWord.innerHTML = randomWordArray[0].toUpperCase();
     nextWord.classList.add('next-word');
     gameContainer.appendChild(nextWord);
+
+    nextWordCount = randomWordArray[0];
+
+    gameContainer.appendChild(wordContainer);
+    for(let i = 0; i < nextWordCount.length; i++){
+        let emptyLetterBox = document.createElement('div');
+        emptyLetterBox.classList = ('letter-box');
+        wordContainer.appendChild(emptyLetterBox);
+    }
 
     let resetBtn = document.createElement('div');
     resetBtn.innerHTML = 'Restart';
     resetBtn.classList.add('restart-btn');
     gameContainer.appendChild(resetBtn);
+
+    userInput.focus();
+
 }
 
 function createNextWord(){
